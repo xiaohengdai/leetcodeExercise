5837611485ea76acc77762b03f77e5c85a176262
xiaohengdai <Xh1013005772>
2022-01-09 15:37:46 +0800

    增加Python 直接赋值、浅拷贝和深度拷贝解析

---
+++ python/python反射.py
@@ -0,0 +1,2 @@
+# Python 反射机制解析：https://blog.csdn.net/u014028063/article/details/80819977
+#Python中反射：https://blog.csdn.net/qq_36883141/article/details/90548486
---
+++ 图像识别/面试前需仔细阅读文章.py
@@ -0,0 +1,4 @@
+# https://www.yuque.com/docs/share/181ad97e-3706-4729-85d2-3bec1a5d9c4b?# 《使用特征匹配以及OCR实现UI自动化对页面样式检查的方法》
+#python中图像识别比较著名的两个包： opencv（读的是BGR顺序）和skimage（读的是RGB顺序）
+#基于python的PIL、OpenCV、SKImage图像转换：https://zhuanlan.zhihu.com/p/139738182
+#perfdog双端性能数据采集原理:https://www.yuque.com/docs/share/28d8eec9-7317-4380-9c27-c2d44bcfe08e?# 《音视频移动端性能采集平台介绍》
